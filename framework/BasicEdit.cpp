4d8c5a07f8eb16fefc2dad0797464ad4fa241f8c
MaximuSG140 <maximstepanov10092002@gmail.com>
2022-08-08 21:05:15 +0700

    Implemented edit draw method

--- framework/BasicEdit.cpp
+++ framework/BasicEdit.cpp
@@ -1,6 +1,31 @@
 #include "pch.h"
 #include "BasicEdit.h"
 
+BasicEdit::BasicEdit(const std::string& name,
+                     const sf::Vector2i position,
+                     const sf::Vector2u size)
+	:Widget("Edit " + name,
+		position,
+		size)
+{}
+
+BasicEdit::BasicEdit(const sf::Vector2i position,
+                     const sf::Vector2u size)
+		:Widget("Unnamed edit", 
+			position,
+			size)
+{}
+
+void BasicEdit::setText(std::string new_text)
+{
+	text_.swap(new_text);
+}
+
+std::string BasicEdit::text() const
+{
+	return text_;
+}
+
 bool BasicEdit::containsCursor(const sf::Vector2i point_coordinates) const
 {
 	auto button_position = position();
@@ -11,4 +36,76 @@ bool BasicEdit::containsCursor(const sf::Vector2i point_coordinates) const
 	auto right_border = button_position.x + button_size.x;
 	return point_coordinates.x >= left_border && point_coordinates.x < right_border &&
 		point_coordinates.y >= upper_border && point_coordinates.y < bottom_border;
+}
+
+void BasicEdit::handleKeyPress(const sf::Event::KeyEvent& event)
+{
+	
+}
+
+bool BasicEdit::selected() const
+{
+	return editing_;
+}
+
+void BasicEdit::draw(sf::RenderTarget& target,
+                     sf::RenderStates states) const
+{
+	auto edit_size = size();
+	sf::RectangleShape body({static_cast<float>(edit_size.x),
+		static_cast<float>(edit_size.y)});
+	auto edit_position = position();
+	body.setPosition({ static_cast<float>(edit_position.x),
+		static_cast<float>(edit_position.y) });
+	body.setFillColor(BODY_COLOR);
+	sf::Text printable_text(text_, sf::Font(), calculateLetterSize());
+	auto text_position = calculateTextPosition();
+	printable_text.setPosition({ static_cast<float>(text_position.x),
+		static_cast<float>(text_position.y) });
+	printable_text.setFillColor(TEXT_COLOR);
+	if(editing_)
+	{
+		body.setOutlineColor(SELECTED_OUTLINE_COLOR);
+	}
+	else
+	{
+		body.setOutlineColor(OUTLINE_COLOR);
+	}
+	body.setOutlineThickness(OUTLINE_THICKNESS);
+	target.draw(body);
+	target.draw(printable_text);
+}
+
+unsigned BasicEdit::calculateLetterSize() const
+{
+	auto edit_size = size();
+	unsigned left_bound = 1;
+	unsigned right_bound = std::min(edit_size.x / 2,
+		edit_size.y / 2);
+	while(right_bound - left_bound > 1)
+	{
+		auto new_bound = (left_bound + right_bound) / 2;
+		sf::Text test_text(text_, sf::Font(), new_bound);
+		if(auto text_bounds = test_text.getLocalBounds(); 
+			text_bounds.width < static_cast<float>(edit_size.x) && 
+			text_bounds.height < static_cast<float>(edit_size.y) / 2)
+		{
+			left_bound = new_bound;
+		}
+		else
+		{
+			right_bound = new_bound;
+		}
+	}
+	return left_bound;
+}
+
+sf::Vector2i BasicEdit::calculateTextPosition() const
+{
+	auto edit_position = position();
+	auto edit_size = size();
+	sf::Text test_text(text_, sf::Font(), calculateLetterSize());
+	return { edit_position.x,
+		static_cast<int>(static_cast<float>(edit_position.y) + 
+			static_cast<float>(edit_size.y) / 2 - test_text.getLocalBounds().height / 2) };
 }
--- framework/BasicEdit.h
+++ framework/BasicEdit.h
@@ -19,14 +19,24 @@ public:
     [[nodiscard]] std::string text()const;
 
     [[nodiscard]] bool containsCursor(sf::Vector2i point_coordinates) const override;
+
+	void handleKeyPress(const sf::Event::KeyEvent& event) override;
+    [[nodiscard]] bool selected() const override;
 protected:
     void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
+	void onSelect() override;
+    void onUnSelect() override;
 public:
     inline static const sf::Color OUTLINE_COLOR{ 220, 220, 220 };
+    inline static const sf::Color SELECTED_OUTLINE_COLOR{ 200, 200, 200 };
     inline static const sf::Color BODY_COLOR{240, 240, 240};
+    inline static const sf::Color TEXT_COLOR{0, 0, 0};
     constexpr static int OUTLINE_THICKNESS = 3;
 private:
+    [[nodiscard]] unsigned calculateLetterSize()const;
+    [[nodiscard]] sf::Vector2i calculateTextPosition()const;
+
     std::string text_;
-    bool editing_;
+    bool editing_ = false;
 };
 
