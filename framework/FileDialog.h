7a3c9a7605a0703f79ec37398d633fe043872132
Максим <maximstepanov10092002@gmail.com>
2022-08-22 13:39:24 +0700

    Tuned gui

--- framework/FileDialog.h
+++ framework/FileDialog.h
@@ -9,8 +9,8 @@ public:
 	static constexpr int WINDOW_HEIGHT = 600;
 	static constexpr int WINDOW_WIDTH = 800;
 
-	static constexpr float EXPLORER_PROPORTION_HEIGHT = 0.75f;
-	static constexpr float EXPLORER_PROPORTION_WIDTH = 0.75f;
+	static constexpr float EXPLORER_PROPORTION_HEIGHT = 0.8f;
+	static constexpr float EXPLORER_PROPORTION_WIDTH = 0.97f;
 	static constexpr unsigned EXPLORER_WIDTH = WINDOW_WIDTH * EXPLORER_PROPORTION_WIDTH;
 	static constexpr unsigned EXPLORER_HEIGHT = WINDOW_HEIGHT * EXPLORER_PROPORTION_HEIGHT;
 
@@ -22,8 +22,9 @@ public:
 
 	static constexpr int BUTTON_OFFSET = 5;
 
-	static constexpr int EXPLORER_TOP = (WINDOW_HEIGHT - EXPLORER_HEIGHT) / 2;
-	static constexpr int EXPLORER_LEFT = (WINDOW_WIDTH - EXPLORER_WIDTH) / 2;
+	static constexpr int EXPLORER_TOP = 0;
+	static constexpr int EXPLORER_LEFT = static_cast<int>(WINDOW_WIDTH) / 2 - 
+		static_cast<int>(EXPLORER_WIDTH) / 2;
 
 	static constexpr int OK_BUTTON_TOP = EXPLORER_TOP + EXPLORER_HEIGHT +
 		FileExplorer::OUTLINE_THICKNESS + SimpleButton::OUTLINE_THICKNESS;
